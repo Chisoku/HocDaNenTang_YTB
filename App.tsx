95174e724b150afcb216875cf3a85be611dbb7b8
Nguyen Khanh Chi <136489069+Chisoku@users.noreply.github.com>
2025-01-02 10:25:23 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  //jsx
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.header}>hoidanit1</Text>
+        <Text style={styles.parent}>
+          hoidanit2
+          <Text style={styles.child}>bla bla</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}>Hello World 1</Text>
+      <Text>Hello World 2</Text>
+    </View>
+  );
+}
+
+//css in js
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  hello1: {
+    color: "red", fontSize: 60,
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  parent: {
+    fontSize: 60,
+    color: "green",
+  },
+  child: {
+    fontSize: 30,
+    color: "pink",
+  }
+});
